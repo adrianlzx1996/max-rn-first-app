caf0ca08fa23cd6b2d7712d379d0fb5f48d3b134
Adrian Leong <leongadrian36@gmail.com>
2019-10-06 21:13:20 +0800

    small adjustment

--- App.js
+++ App.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+	const [outputText, setOutputText] = useState(
+		"Open up App.js to start working on your app!"
+	);
+	return (
+		<View style={styles.container}>
+			<Text>{outputText}</Text>
+			<Button
+				title="Change Text"
+				onPress={() => setOutputText("Text Changed!")}
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center"
+	}
 });
